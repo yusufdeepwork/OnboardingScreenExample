7929a86727973d0c8e752e91e3624815bf7f70da
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-09 22:31:59 +0300

    First Launch configuration that related Onboarding Screen has been added.

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import LoginScreen from './screens/LoginScreen';
 import OnboardingScreen from './screens/OnboardingScreen';
-
+import AsyncStorage from '@react-native-community/async-storage';
 const AppStack = createStackNavigator();
 
 const App = () => {
-  return (
-    <NavigationContainer>
-      <AppStack.Navigator headerMode="none">
-        <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
-        <AppStack.Screen name="Login" component={LoginScreen} />
-      </AppStack.Navigator>
-    </NavigationContainer>
-  );
+  const [isFirstLaunch, setIsFirstLaunch] = React.useState(null);
+
+  useEffect(() => {
+    AsyncStorage.getItem('alreadyLaunched').then((value) => {
+      if (value == null) {
+        AsyncStorage.setItem('alreadyLaunched', 'true');
+        setIsFirstLaunch(true);
+      } else {
+        setIsFirstLaunch(false);
+      }
+    });
+  });
+
+  if (isFirstLaunch === null) {
+    return null;
+  } else if (isFirstLaunch === true) {
+    return (
+      <NavigationContainer>
+        <AppStack.Navigator headerMode="none">
+          <AppStack.Screen name="Onboarding" component={OnboardingScreen} />
+          <AppStack.Screen name="Login" component={LoginScreen} />
+        </AppStack.Navigator>
+      </NavigationContainer>
+    );
+  } else {
+    return <LoginScreen />;
+  }
 };
 
 export default App;
