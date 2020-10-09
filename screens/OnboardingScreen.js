f1428ebb88e6c19d890ef3e6fc0775fc09152bb2
yusufdeepwork <yusuf.deepwork@gmail.com>
2020-10-09 21:54:53 +0300

    Dot Component has been added.

--- screens/OnboardingScreen.js
+++ screens/OnboardingScreen.js
@@ -9,6 +9,16 @@ import {
 } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
 
+const Dots = ({selected}) => {
+  let backgroundColor;
+
+  backgroundColor = selected ? 'rgba(0, 0, 0, 0.8)' : 'rgba(0, 0, 0, 0.3)';
+
+  return (
+    <View style={{width: 6, height: 6, marginHorizontal: 3, backgroundColor}} />
+  );
+};
+
 const Skip = (props) => {
   <Button title="Skip" color="#000000" {...props} />;
 };
@@ -17,9 +27,8 @@ const Next = (props) => {
   <Button title="Next" color="#000000" {...props} />;
 };
 const Done = ({props}) => {
-  <TouchableOpacity style={{marginHorizontal: 8}} {...props} >
-
-    <Text style={{fontSize:16}}>Done</Text>
+  <TouchableOpacity style={{marginHorizontal: 8}} {...props}>
+    <Text style={{fontSize: 16}}>Done</Text>
   </TouchableOpacity>;
 };
 
@@ -29,6 +38,7 @@ const OnboardingScreen = ({navigation}) => {
       SkipButtonComponent={Skip}
       NextButtonComponent={Next}
       DoneButtonComponent={Done}
+      DotComponent={Dots}
       // We must'nt go previous page when it is finished.
       onSkip={() => navigation.replace('Login')}
       onDone={() => navigation.navigate('Login')}
